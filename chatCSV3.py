0d9c8703386dea48a20fbd582f72db74ee8d5479
ravi6389 <77967899+ravi6389@users.noreply.github.com>
2024-08-12 12:33:34 +0530

    Update chatCSV3.py

--- chatCSV3.py
+++ chatCSV3.py
@@ -12,8 +12,6 @@ from langchain_groq import ChatGroq
 import ssl
 context = ssl._create_unverified_context()
 
-print("I am in chatcsv2")
-
 
 
 
@@ -24,7 +22,7 @@ llm = ChatGroq(temperature=0, groq_api_key=st.secrets['GROQ_API_KEY'], model_nam
 
 
 st.header('Analyze CSV file with Parchi - a GenAI tool for chatting with CSV')
-st.subheader('Developed by Ravi Shankar Prasad - https://www.linkedin.com/in/ravi-shankar-prasad-371825101')
+st.write('Developed by Ravi Shankar Prasad - https://www.linkedin.com/in/ravi-shankar-prasad-371825101')
 
 uploaded_file = st.file_uploader('Upload your CSV file')
 
