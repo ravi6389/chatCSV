7719a1d1f6ad0ae3f787d3fba8edafb07ee896b0
ravi6389 <77967899+ravi6389@users.noreply.github.com>
2024-09-20 23:43:28 +0530

    Update chatCSV3.py

--- chatCSV3.py
+++ chatCSV3.py
@@ -27,7 +27,7 @@ st.write('Developed by Ravi Shankar Prasad - https://www.linkedin.com/in/ravi-sh
 uploaded_file = st.file_uploader('Upload your CSV file')
 
 if(uploaded_file):
-    df = pd.read_csv(uploaded_file, encoding='unicode_escape')
+    df = pd.read_excel(uploaded_file)
 
     st.write(df.head(3))
 
